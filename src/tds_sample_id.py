24be0dae89c2da20eede1f0993a0f9ce2c12767f
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-09-27 16:37:55 +0200

    Rename file, change from class to function.

--- src/tds_sample_id.py
+++ src/tds_sample_id.py
@@ -6,14 +6,12 @@ import base64
 #USED_CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-class TDSSampleID:
-    def __intit__(self):
+def tds_gen_new_id() -> str:
         t1: bytes = int(time.time()).to_bytes(5, byteorder="big")
         t2: str = str(base64.b32hexencode(t1))
 
         b1: bytes = random.randbytes(40)
         b2: str = str(base64.b32hexencode(b1))
 
-        self.id = f"{t2}_{b2}"
-
+        return f"{t2}_{b2}"
 
