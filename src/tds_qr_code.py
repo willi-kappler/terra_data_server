5b63654af1f8b2f1516d9d8c0af9a7a12bb66b11
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-09-27 16:38:15 +0200

    Add function to generate qr code.

---
+++ src/tds_qr_code.py
@@ -0,0 +1,13 @@
+
+import segno
+
+# URL:
+# https://terra-data.guz.uni-tuebingen.de/update_sample/xxxx
+# https://teda.guz.uni-tuebingen.de/update_sample/xxxx
+
+
+def tds_gen_qr_code(id: str) -> None:
+    url = f"https://scripts.guz.uni-tuebingen.de/teda/update_sample/{id}"
+    qrcode = segno.make_qr(url, error="H")
+    qrcode.save(f"{id}.png")
+
